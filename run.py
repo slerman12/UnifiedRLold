5aef86d29e2025be532bb178aa8cbb62fca2bbdf
samlerman <slerman@ur.rochester.edu>
2021-10-21 13:30:39 -0400

    submitit

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import hydra
 from pathlib import Path
+from omegaconf import DictConfig
 
 
 @hydra.main(config_path='hyperparams', config_name='atari_drqdqn')
-def main(cfg):
+def main(cfg: DictConfig) -> None:
     from train import Workspace as W
     root_dir = Path.cwd()
     workspace = W(cfg)
