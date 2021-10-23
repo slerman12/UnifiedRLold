b59ee058e3dd2dc0c3e62ac797ccf6fcbb96927a
samlerman <slerman@ur.rochester.edu>
2021-10-23 19:39:58 -0400

    longer sleep

--- atari_cpu_sweep.sh
+++ atari_cpu_sweep.sh
@@ -5,6 +5,14 @@ for game in "alien" "amidar" "assault" "asterix" "bankheist" "battlezone" "boxin
 #for game in "alien" "amidar" "assault" "asterix" "bank_heist" "boxing" "breakout" "chopper_command"
 #for game in "breakout"
 do
+  {
 	python3 sbatch.py --bigger-gpu --name $game$seed --params "--config-name atari task=atari/$game seed=$seed experiment=$game$seed"
+	} || {
+	python3 sbatch.py --bigger-gpu --name $game$seed --params "--config-name atari task=atari/$game seed=$seed experiment=$game$seed"
+	echo "saved"
+	} || {
+	python3 sbatch.py --bigger-gpu --name $game$seed --params "--config-name atari task=atari/$game seed=$seed experiment=$game$seed"
+	echo "saved backup"
+	}
 	sleep 7
 done
