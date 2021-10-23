52aedbdc41a2e31284a03002c081d2304843558f
samlerman <slerman@ur.rochester.edu>
2021-10-23 16:13:18 -0400

    atari

---
+++ hyperparams/task/generate_rainbow.py
@@ -0,0 +1,14 @@
+RAINBOW_ENVS = [
+    'Alien', 'Amidar', 'Assault', 'Asterix', 'BankHeist', 'BattleZone',
+    'Boxing', 'Breakout', 'ChopperCommand', 'CrazyClimber', 'DemonAttack',
+    'Freeway', 'Frostbite', 'Gopher', 'Hero', 'Jamesbond', 'Kangaroo', 'Krull',
+    'KungFuMaster', 'MsPacman', 'Pong', 'PrivateEye', 'Qbert', 'RoadRunner',
+    'Seaquest', 'UpNDown'
+]
+
+for env in RAINBOW_ENVS:
+    f = open(f"./atari/{env.lower()}.yaml", "w")
+    f.write(r"""num_train_frames:  100001
+stddev_schedule: 'linear(1.0,0.1,500000)'
+task_name: {}""".format(env))
+    f.close()
