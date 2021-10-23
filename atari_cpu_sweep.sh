1d9ef83c7151ebc24586af77517b66543d56ec11
samlerman <slerman@ur.rochester.edu>
2021-10-23 19:10:45 -0400

    atari

--- atari_cpu_sweep.sh
+++ atari_cpu_sweep.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-module load python3/3.8.3
+#module load python3/3.8.3
 seed=$1
-for game in "alien" "amidar" "assault" "asterix" "bank_heist" "battle_zone" "boxing" "breakout" "chopper_command" "crazy_climber" "demon_attack" "freeway" "frostbite" "gopher" "hero" "jamesbond" "kangaroo" "krull" "kung_fu_master" "ms_pacman" "pong" "private_eye" "qbert" "road_runner" "seaquest" "up_n_down"
+for game in "alien" "amidar" "assault" "asterix" "bankheist" "battlezone" "boxing" "breakout" "choppercommand" "crazyclimber" "demonattack" "freeway" "frostbite" "gopher" "hero" "jamesbond" "kangaroo" "krull" "kungfumaster" "mspacman" "pong" "privateeye" "qbert" "roadrunner" "seaquest" "upndown"
 #for game in "alien" "amidar" "assault" "asterix" "bank_heist" "boxing" "breakout" "chopper_command"
 #for game in "breakout"
 do
-  source /scratch/slerman/miniconda/bin/activate agi
 	python3 sbatch.py --bigger-gpu --name $game$seed --params "--config-name atari task=atari/$game seed=$seed experiment=$game$seed"
 	sleep 3
 done
--- hyperparams/task/generate_rainbow.py
+++ hyperparams/task/generate_rainbow.py
@@ -6,9 +6,12 @@ RAINBOW_ENVS = [
     'Seaquest', 'UpNDown'
 ]
 
+out = ""
 for env in RAINBOW_ENVS:
     f = open(f"./atari/{env.lower()}.yaml", "w")
     f.write(r"""num_train_frames:  100001
 stddev_schedule: 'linear(1.0,0.1,500000)'
 task_name: {}""".format(env))
     f.close()
+    out += ' "' + env.lower() + '"'
+print(out)
