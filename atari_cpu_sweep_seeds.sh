cc17e43b83efa678057d15125d6b36f66f9ceed2
samlerman <slerman@ur.rochester.edu>
2021-10-23 19:45:11 -0400

    longer sleep

--- atari_cpu_sweep_seeds.sh
+++ atari_cpu_sweep_seeds.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 #module load python3/3.8.3
 numseeds=$1
-sleeptime='expr $numseeds * 7 + 7'
 for i in $(seq 1 $numseeds)
 #for game in "alien" "amidar" "assault" "asterix" "bank_heist" "boxing" "breakout" "chopper_command"
 #for game in "breakout"
 do
 	bash atari_cpu_sweep.sh $i
-	sleep $sleeptime
 done
