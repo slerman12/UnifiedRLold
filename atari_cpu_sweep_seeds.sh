13b47cc1da35300324c360bf5549d6f01280d064
samlerman <slerman@ur.rochester.edu>
2021-10-27 02:38:25 -0400

    error catch in sweep

--- atari_cpu_sweep_seeds.sh
+++ atari_cpu_sweep_seeds.sh
@@ -2,6 +2,7 @@
 #module load python3/3.8.3
 numseeds=$1
 experiment=$2
+echo "experiment $experiment"
 for i in $(seq 1 $numseeds)
 #for game in "alien" "amidar" "assault" "asterix" "bank_heist" "boxing" "breakout" "chopper_command"
 #for game in "breakout"
