aa59f5d307ed4b64fe173579e92c873917dc7e74
samlerman <slerman@ur.rochester.edu>
2021-10-27 18:40:33 -0400

    memory sbatch

--- sbatch.py
+++ sbatch.py
@@ -14,6 +14,9 @@ parser.add_argument('--bigger-gpu', action='store_true', default=False,
                     help='uses K80 GPU')
 parser.add_argument('--biggest-gpu', action='store_true', default=False,
                     help='uses V100 GPU')
+parser.add_argument('--very-biggest-gpu', action='store_true', default=False,
+                    help='uses A100 GPU')
+parser.add_argument('--mem', type=int, default=25)
 parser.add_argument('--file', type=str, default="run.py")
 parser.add_argument('--params', type=str, default="task=atari/ms_pacman")
 args = parser.parse_args()
@@ -25,7 +28,7 @@ def slurm_script_generalized():
 {}
 {}
 #SBATCH -t {} -o ./{}.log -J {}
-#SBATCH --mem=25gb 
+#SBATCH --mem={}gb 
 {}
 
 source /scratch/slerman/miniconda/bin/activate agi
@@ -35,7 +38,8 @@ python3 {} {}
            "#SBATCH -p csxu -A cxu22_lab" if args.cpu and args.lab else "#SBATCH -p csxu -A cxu22_lab --gres=gpu" if args.lab else "",
            "15-00:00:00" if args.lab else "5-00:00:00",
            args.name, args.name,
-           "#SBATCH -C K80" if args.bigger_gpu else "#SBATCH -C V100" if args.biggest_gpu else "",
+           args.mem,
+           "#SBATCH -C K80" if args.bigger_gpu else "#SBATCH -C V100" if args.biggest_gpu else "#SBATCH -C A100" if args.very_biggest_gpu else "",
            args.file, args.params)
 
 
