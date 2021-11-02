722992955f38addedfc697d4d0070fc72cf5c5fb
samlerman <slerman@ur.rochester.edu>
2021-11-01 20:00:34 -0400

    humanoid sweep seeds 2 - 5

--- humanoid_cpu_sweep_seeds.sh
+++ humanoid_cpu_sweep_seeds.sh
@@ -3,7 +3,7 @@
 numseeds=$1
 experiment=$2
 echo "experiment $experiment"
-for i in $(seq 2 $numseeds)
+for i in $(seq 1 $numseeds)
 #for game in "alien" "amidar" "assault" "asterix" "bank_heist" "boxing" "breakout" "chopper_command"
 #for game in "breakout"
 do
