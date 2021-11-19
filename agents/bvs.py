c0f5b975ca323bee864d458a1aa5bc6c2a036dcd
samlerman <slerman@ur.rochester.edu>
2021-11-19 18:27:48 -0500

    bvs

--- agents/bvs.py
+++ agents/bvs.py
@@ -135,7 +135,7 @@ class BVSAgent:
         metrics = dict()
 
         # for now, do 2-step only todo
-        all_obs = all_obs[:, 0:2]
+        all_obs = torch.cat([all_obs[:, 0], all_obs[:, 1]], dim=0)
 
         next_obs = self.aug(all_obs[:, 1:].float())
 
