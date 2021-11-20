e1a3a3c2cbba64c7452ee5b5ae180e9fa30174b6
samlerman <slerman@ur.rochester.edu>
2021-11-20 17:10:46 -0500

    pro

--- agents/pro.py
+++ agents/pro.py
@@ -69,14 +69,13 @@ class PROAgent:
         obs = self.encoder(obs.unsqueeze(0))
         stddev = utils.schedule(self.stddev_schedule, step)
         dist = self.actor(obs, stddev)
+        print(dist.mean, stddev)
         if eval_mode:
             action = dist.mean
         else:
             action = dist.sample(clip=None)
-            print(action)
             if step < self.num_expl_steps:
                 action.uniform_(-1.0, 1.0)
-            print(action)
         # continuous vs discrete
         if self.discrete:
             return torch.argmax(action, dim=-1).cpu().numpy()[0]
